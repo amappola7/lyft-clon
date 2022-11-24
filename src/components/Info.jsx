9d5d001117130ea48ebbc3dd1360a91bd34d9274
Amappola <anam.porrasp@gmail.com>
2022-11-24 06:33:48 -0500

    Maquetación info. terminada

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,23 @@
 
 function Info() {
   return (
-    <section />
+    <section className="info">
+      <div info__content>
+        <div className="info__text-content">
+          <h1>A RIDE IN MINUTES</h1>
+          <p>Request a ride and you'll be on your way in minutes.<br />Request. Ride. Repeat.</p>
+        </div>
+        <div className="info__text-content">
+          <h1>SERIOUS ABOUT SAFETY</h1>
+          <p>From knowing the color of your driver's car to our 24/7<br />Trust & Safety Team, we got you.</p>
+        </div>
+        <div className="info__text-content">
+          <h1>HAPPY DRIVERS.<br />HAPPY RIDERS.</h1>
+          <p>Ride with us and you'll see why 9 out of 10 rides end with five<br />stars.</p>
+        </div>
+      </div>
+      <img src="../assets/phone.png" alt="Cellphone" />
+    </section>
   )
 }
 
